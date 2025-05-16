db8acc46d80529a08ed0c829c681ade5102341ce
rithika2705 <rithikashetty356@gmail.com>
2025-05-16 10:14:58 +0530

    Update program1b.java

--- program1b.java
+++ program1b.java
@@ -1,5 +1,5 @@
-// 1b programs
-// 1) Write a java program for getting different colors through ArrayList interface and search whether the color "Red" is available or not
+//********************** 1b programs******************************
+// -------> 1) Write a java program for getting different colors through ArrayList interface and search whether the color "Red" is available or not
 
 package array;
 import java.util.*;
@@ -23,3 +23,43 @@ public static void main(String[] args) {
 		        }
 		    }
 		}
+
+
+
+
+//------------> 2) Write a java program for getting different colors through ArrayList interface and remove the 2nd element and color "Blue" from the ArrayList
+
+package array;
+import java.util.*;
+public class p1 {
+public static void main(String[] args) {
+	        // Create an ArrayList to store colors
+	        ArrayList<String> c = new ArrayList<>();
+	        // Add colors to the list
+	        c.add("Red");
+	        c.add("Blue");
+	        c.add("Green");
+	        c.add("Yellow");
+	        c.add("Purple");
+	        // Display the original list
+	        System.out.println("Original list: " + c);
+	        // Remove the second element (index 1)
+	        if (c.size() > 1) {
+	            String removedElement = c.remove(1);
+	            System.out.println("Removed element at index 1: " + removedElement);
+	        } else {
+	            System.out.println("List has fewer than 2 elements; cannot remove index 1.");
+	        }
+
+	        // Remove the color "Blue" from the list
+	        boolean isBlueRemoved = c.remove("Blue");
+	        if (isBlueRemoved) {
+	            System.out.println("Removed 'Blue' from the list.");
+	        } else {
+	            System.out.println("'Blue' was not found in the list.");
+	        }
+
+	        // Display the updated list
+	        System.out.println("Updated list: " + c);
+	    }
+	}
